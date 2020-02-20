58dd0dabba4988b8a5141b2e47084b534cedc228
Leandro <Francisco@192.168.1.53>
2020-02-19 19:54:31 -0500

    Merge origin/master
    Conflicts:
    	src/ventanas/Inicio.java
    	src/ventanas/InicioEstudiantes.form
    	src/ventanas/InicioEstudiantes.java

--- src/ventanas/Profesores_Area_Edicion_Notas.java
+++ src/ventanas/Profesores_Area_Edicion_Notas.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ventanas;
 
 /**
- *
  * @author cricr
  */
 public class Profesores_Area_Edicion_Notas extends javax.swing.JFrame {
@@ -37,13 +31,13 @@ public class Profesores_Area_Edicion_Notas extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jLabel_FondoProfesoresAreaEdicion)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 1000, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jLabel_FondoProfesoresAreaEdicion)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 600, Short.MAX_VALUE))
         );
 
         pack();
