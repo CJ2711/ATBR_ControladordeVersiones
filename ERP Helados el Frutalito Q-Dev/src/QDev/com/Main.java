8ae0fffeec0490b47a1809674b612d0ae7999e9d
Christian <cricriman7@gmail.com>
2021-03-02 23:52:23 -0500

    bang actualizado

--- ERP Helados el Frutalito Q-Dev/src/QDev/com/Main.java
+++ ERP Helados el Frutalito Q-Dev/src/QDev/com/Main.java
@@ -5,6 +5,7 @@
  */
 package QDev.com;
 
+import QDev.com.GUI.GUI_Admin;
 import QDev.com.GUI.GUI_Login;
 import QDev.com.GUI.GUI_UserManagement;
 
@@ -60,6 +61,26 @@ public class Main {
             java.util.logging.Logger.getLogger(GUI_UserManagement.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(GUI_UserManagement.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+                /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI_Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI_Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI_Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI_Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         
         //</editor-fold>
