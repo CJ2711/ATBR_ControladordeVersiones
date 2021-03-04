89434986d5bea6cfc576a674c40311153ebed245
Alejandro <Alejandro@AlCR-PC>
2021-03-03 23:25:55 -0500

    Merge origin/main into ALCR1
    Registro de Usuarios Funcional.

--- ERP Helados el Frutalito Q-Dev/src/QDev/com/DB/ConsultaBD.java
+++ ERP Helados el Frutalito Q-Dev/src/QDev/com/DB/ConsultaBD.java
@@ -1,4 +1,3 @@
-
 package QDev.com.DB;
 
 import QDev.com.Classes.Person;
@@ -16,34 +15,34 @@ import java.util.logging.Logger;
  * @author cricr
  * @author Leandro Rodriguez Vidal 1958205-2711
  */
-public class ConsultaBD extends Conectar{ 
-    
+public class ConsultaBD extends Conectar {
+
 //    Conectar cn = new Conectar();
     PreparedStatement pst = null;
     Connection cnn = conexion();
     ResultSet rs = null;
-    
-    public boolean loginUser(Person person){
+
+    public boolean loginUser(Person person) {
         String sql = "SELECT nuip, names, surnames, phone, email, psswrd, active, "
                 + "nameRole FROM person WHERE nuip = ?"; // AND active = ?";
         try {
             pst = (PreparedStatement) cnn.prepareStatement(sql);
             pst.setInt(1, (int) person.getNuip());
 //            pst.setBoolean(2, person.isActive());
-            
+
             rs = pst.executeQuery();
-            
-            if(rs.next()){
-                if(person.getPassword().equals(rs.getString(6))){
+
+            if (rs.next()) {
+                if (person.getPassword().equals(rs.getString(6))) {
                     person.setNames(rs.getString(2));
                     person.setSurnames(rs.getString(3));
                     person.setPhone(rs.getString(4));
                     person.setEmail(rs.getString(5));
                     person.setActive(rs.getBoolean(7));
-                    if("SALESMAN".equals(rs.getString(8))){
+                    if ("SALESMAN".equals(rs.getString(8))) {
                         person.setRole(Role.SALESMAN);
                     }
-                    if("BUSINESS_ADMIN".equals(rs.getString(8))){
+                    if ("BUSINESS_ADMIN".equals(rs.getString(8))) {
                         person.setRole(Role.BUSINESS_ADMIN);
                     }
                     return true;
@@ -51,41 +50,40 @@ public class ConsultaBD extends Conectar{
                     return false;
                 }
             }
-            
+
             return false;
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(ConsultaBD.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
-        
+
     }
-    
+
     //Registrar Usuario
-    public boolean registerUser (Person person) {
+    public boolean registerUser(Person person) {
         String sql = "INSERT INTO person (nuip, names, surnames, phone, email, "
-                + "pssword, active, nameRole) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                + "psswrd, active, nameRole) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             pst = (PreparedStatement) cnn.prepareStatement(sql);
             pst.setInt(1, (int) person.getNuip());
             pst.setString(2, person.getNames());
             pst.setString(3, person.getSurnames());
             pst.setString(4, person.getPhone());
-            pst.setString(5, person.getSurnames());
-            pst.setString(6, person.getEmail());
-            pst.setString(7, person.getPassword());
-            pst.setBoolean(8, person.isActive());
-            pst.setString(9, person.getRole().name());
-            
-            pst.execute(); 
-            
+            pst.setString(5, person.getEmail());
+            pst.setString(6, person.getPassword());
+            pst.setBoolean(7, person.isActive());
+            pst.setString(8, person.getRole().name());
+
+            pst.execute();
+
             return true;
         } catch (SQLException ex) {
             Logger.getLogger(ConsultaBD.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
     }
-    
+
     //Cargar tabla de la interfaz 'GUI_UserManagement'
 //    public void loadUsersTable(){
 //        String [] registries = new String[8];
@@ -110,6 +108,4 @@ public class ConsultaBD extends Conectar{
 //            Logger.getLogger(ConsultaBD.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 //    }
-
-    
 }
